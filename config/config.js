d8aab8a47484a882a844eafbf8a0fe3fffbb086b
renrongzhen <152015820+renrongzhen@users.noreply.github.com>
2023-12-08 19:23:59 +0800

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,118 @@
+var config = {
+    name: "renrongzhen",
+    sex: "man",
+    age: "20",
+    phone: "18136169102",
+    email: "2703596192@qq.com",
+    address: "Wuxi City, Jiangsu Province",
+    qq: "2703596192",
+    log: "Happysnaker",
+    excpect_work: "Cybersecurity",
+
+
+
+    motto: [
+        "Tomorrow will not necessarily be better, but we must firmly believe that a better tomorrow will come.",
+        "There is always time and opportunity to do what you want to do, and there is always an excuse for what you don't want to do.",
+        "Gor For It!",
+        "Those who have wisdom have long aspirations, and those who have no aspirations have long aspirations.",
+        "The tears of the past will eventually dry in the memory.",
+        "The truth is to extinguish illusions.",
+        "If you want to turn your heart to the bright moon, the bright moon shines on the ditch",
+        "The spring breeze is proud, and you can see all the Chang'an flowers in one day.",
+        "It's cool in autumn!",
+        "lao Ji Fu Fang, aiming for a thousand miles. The martyrs are in their twilight years, and their hearts are strong.",
+        "When it is stronger, it is better to move the heart of the white head. Poor and strong, do not fall into the ambition of the clouds.",
+        "We have to trade what we have for what we don't have",
+        "The leaves are pale, and the white dew is frost; The so-called Iraqi people are on the water side.",
+        "Count the romantic characters, and look at the present day!"
+    ],
+
+
+
+    welcome: "Qingqing Zijin, leisurely my heart<br>" +
+        "But for the sake of the king, he has been groaning so far<br>" +
+        "Hello, I'm Renrongzhen, a sophomore majoring in computer science and science at Wuxi Taihu University<br>" +
+        "Nice to see you!",
+
+
+
+    about: "<p>Hello! My name is Renrongzhen, male, sophomore at Wuxi Taihu University. My desired job position is a cybersecurity engineer.</p>" +
+        "<p>I have a lot of experience in python programming, and I have a solid grasp of basic computer knowledge, and I can complete my tasks well in my work. In addition, I have a passionate work attitude, strong team work ability, and I also have the ability to develop independently, and I am good at finding and solving problems. I have strong executive ability, a high sense of responsibility, a strong sense of collective honor, the courage to take responsibility, and the ability to accept overtime or business trips</p>" +
+        "<p>We look forward to hearing from you!</p>",
+
+
+
+
+    skills: [
+        ["Python", 70, "red"],
+        ["c++", 50, "blue"],
+        ["Cybersecurity", 60, "#1abc9c"],
+        ["HTML5", 80, "rgba(0,0,0)"],
+        ["CSS3", 80, "yellow"],
+        ["JavaScript", 80, "pink"]
+    ],
+
+
+
+    skills_description: "<ul>" +
+        "     <li>Basic knowledge of programming such as operating systems, computer networks, etc. is good.</li>" +
+        "     <li>Proficient in Python basics.</li>" +
+        "     <li>Familiarity with how firewalls work between them</li>" +
+        "     <li>Pass CET-4</li>" +
+        "     <li>Pass Level 3 of the Computer Grading Exam</li>" +
+        "     <li>Familiar with SQL statement writing and tuning.</li>" +
+        "     <li>Familiarize yourself with the basics of Go language development.</li>" +
+        "     <li>Familiar with the use of Spring, ibatis, struts and other frameworks, and understand their principles and mechanisms.</li>" +
+        "     <li>Familiarity with caching, messaging, and other mechanisms</li>" +
+        "     <li>Understand the design and application of distributed systems.</li>" +
+        "     <li>Familiarity with HTML, CSS, JavaScript, and the corresponding front-end knowledge.</li>" +
+        " </ul>",
+
+
+
+    portfolio: [
+        ["./images/pro-1.png", "http://1.15.234.109:8000/", "个人博客", "这里记录了我的Java后端学习笔记<br>持续更新"],
+        ["./images/pro-2.png", "https://github.com/happysnaker/Gobang", "智能人机对战五子棋", "采用C++编写的智能五子棋人机对战<br>2021/7/23"],
+        ["https://pic3.zhimg.com/80/v2-d9766956d5c85c2780e4c5008fd946ca_1440w.jpg", "https://github.com/happysnaker/StudentsManageSystem", "学生管理系统", "C语言+AVL树+多重双向表实现"]
+    ],
+
+
+
+    work: [
+        //如果您内有工作经历，您可以采取下列写法
+        ["————————", "", "<p>I have no work experience and look forward to hearing from you.</p>"]
+
+
+    ],
+
+
+
+    others: [
+        ["2023-11-28", "The third prize of the scholarship is awarded by the college", "Won the third prize of the college scholarship"],
+
+        ["2023-10-24", "Computer-based exams", "In the second semester of my freshman year, I passed the national computer grade exam"],
+        ["2023-06-14", "CET-4 exam", "In the second semester of my freshman year, I took the English Level 4 exam"]
+    ],
+
+
+
+    con: [
+        ["./svg/LeetCode.svg", "https://leetcode-cn.com/u/happysnaker/", "我的力扣主页"],
+        ["./svg/github.svg", "https://github.com/happysnaker", "我的GitHub主页"],
+        ["./svg/博客.svg", "http://1.15.234.109:8000", "我的个人博客"],
+        ["./svg/掘金.svg", "https://juejin.cn/user/3853167638625000", "我的掘金主页"],
+        ["./svg/知乎.svg", "https://www.zhihu.com/people/tian-xia-you-dao-81", "我的知乎主页"]
+    ],
+
+
+    //这是一些图片链接，建议您仅更改第二个头像图片
+    url: [
+        //背景图、头像、作品展示背景、其他经历背景
+        "./images/intro-bg.jpg",
+        "./images/111.jpg",
+        "./images/work-bk.png",
+        "./images/4.jpg"
+    ]
+
+}
